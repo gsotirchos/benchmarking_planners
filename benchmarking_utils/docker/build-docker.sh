f749fea137298a0fedfc4fc9b72b2ad0766cec19
gsotirchos <gsotirchos@student.tudelft.nl>
2024-01-06 20:28:26 +0100

    Update docker to ADD locally cloned parent repo

--- benchmarking_utils/docker/build-docker.sh
+++ benchmarking_utils/docker/build-docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-cd "${0%/*}" \
-    && sudo docker build -t benchmarking .
-#    &&  sudo docker buildx build -t benchmarking .
+cd "${0%/*}/../.." && \
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        docker build -t benchmarking -f benchmarking_utils/docker/Dockerfile .
+    else
+        sudo docker build -t benchmarking -f benchmarking_utils/docker/Dockerfile .
+    fi

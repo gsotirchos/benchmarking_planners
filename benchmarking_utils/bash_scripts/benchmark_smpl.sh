aedc9e3c7c10d1d804993e4aafcdd224b21d7418
gsotirchos <gsotirchos@student.tudelft.nl>
2024-02-12 09:52:11 +0100

    Add planners & right test datasets

--- benchmarking_utils/bash_scripts/benchmark_smpl.sh
+++ benchmarking_utils/bash_scripts/benchmark_smpl.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 
 prefix_dset="$(rospack find pyre)/datasets/"
-datasets=("shelf_zero" "shelf_height" "shelf_height_rot")
+planners=(
+    "arastar"
+    # "awastar"
+    # "mhastar"
+    # "larastar"
+    # "egwastar"
+    # "padastar"
+)
+datasets=(
+    "shelf_zero_test"
+    # "shelf_height_test"
+    # "shelf_height_rot_test"
+)
 start=1
 end=100
 
-for dset in "${datasets[@]}"; do
-    dataset="${prefix_dset}/${dset}"
+for planner in "${planners[@]}"; do
+    for dset in "${datasets[@]}"; do
+        dataset="${prefix_dset}/${dset}"
 
-    roslaunch smpl_test goal_fetch_benchmarking.launch start:="${start}" end:="${end}" dataset:="${dataset}"
+        roslaunch smpl_test goal_fetch_benchmarking.launch \
+            start:="${start}" \
+            end:="${end}" \
+            dataset:="${dataset}" \
+            planner:="${planner}"
+    done
 done

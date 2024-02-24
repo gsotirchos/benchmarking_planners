d869acbd02e825053aaf293a36296b81bc313a86
gsotirchos <gsotirchos@student.tudelft.nl>
2024-02-24 12:42:17 +0100

    Improve automated building

--- benchmarking_utils/bash_scripts/build-apptainer.sh
+++ benchmarking_utils/bash_scripts/build-apptainer.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
+user_specified_path="$1"
+container_path="${user_specified_path:-"${HOME}"/benchmarking}"
 ws_path="$(dirname "$(realpath "$0")")"/../../../..
-container_path="${HOME}"/benchmarking
 
-if [[ ! -d "${container_path}" ]]; then
+if [[ ! -f "${container_path}"/singularity ]]; then
     apptainer build \
         --bind "${ws_path}":/opt/catkin_ws \
         --sandbox "${container_path}" \
         "${ws_path}"/src/benchmarking_planners/benchmarking_utils/apptainer/benchmarking.def
-fi
-
-apptainer exec \
-    --writable "${container_path}" \
-    "${ws_path}"/src/benchmarking_planners/benchmarking_utils/bash_scripts/build-ws.sh
-
-if [[ ! -f "${ws_path}"/src/benchmarking_planners/robowflex_resources/fetch/robots/fetch.backup ]]; then
+else
     apptainer exec \
         --writable "${container_path}" \
-        "${ws_path}"/src/benchmarking_planners/benchmarking_utils/bash_scripts/post-build.sh
+        "${ws_path}"/src/benchmarking_planners/benchmarking_utils/bash_scripts/build-ws.sh
+
+    if [[ ! -f "${ws_path}"/src/benchmarking_planners/robowflex_resources/fetch/robots/fetch.backup ]]; then
+        apptainer exec \
+            --writable "${container_path}" \
+            "${ws_path}"/src/benchmarking_planners/benchmarking_utils/bash_scripts/post-build.sh
+    fi
 fi
